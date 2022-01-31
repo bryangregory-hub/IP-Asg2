4fdbe85d5d8e68fffe3a8505db00f2a4c1330113
bryangregory-hub <73159622+bryangregory-hub@users.noreply.github.com>
2022-02-01 00:19:30 +0800

    object done

--- IP asg 2/Assets/Scripts/HandPresence.cs
+++ IP asg 2/Assets/Scripts/HandPresence.cs
@@ -4,46 +4,73 @@ using UnityEngine;
 using UnityEngine.XR;
 public class HandPresence : MonoBehaviour
 {
+    public bool showContoller = false;
+    public InputDeviceCharacteristics controllerCharacteristics;
+    public List<GameObject> controllerPrefabs;
+    public GameObject handModelPrefab;
+
     private InputDevice targetDevice;
+    private GameObject spawnedController;
+    private GameObject spawnedHandModel;
+    private bool calledOnce = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(GetDevices(5));
     }
-     void FixedUpdate()
+    
+    IEnumerator GetDevices(float delayTime)
     {
+        yield return new WaitForSeconds(delayTime);
         List<InputDevice> devices = new List<InputDevice>();
-        InputDeviceCharacteristics rightControllerCharacteristics = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
-        InputDevices.GetDevicesWithCharacteristics(rightControllerCharacteristics, devices);
+        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
         foreach (var item in devices)
         {
             Debug.Log(item.name + item.characteristics);
         }
-        if (devices.Count>0)
+        if (devices.Count > 0)
         {
             targetDevice = devices[0];
+
+            spawnedHandModel = Instantiate(handModelPrefab, transform);
+            calledOnce = true;
         }
     }
+    void FixedUpdate()
+    {
+        
+            
+        
+        
+    }
+
     // Update is called once per frame
     void Update()
     {
+        
+
         targetDevice.TryGetFeatureValue(CommonUsages.secondary2DAxisClick, out bool primaryButtonValue);
         if (primaryButtonValue)
         {
+            
             Debug.Log("Pressing Primary Button");
         }
         
         targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue);
         if (triggerValue > 0.1f)
         {
+            
             Debug.Log("Pressing trigger Button" + triggerValue);
 
         }
         targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 Primary2D);
         if (Primary2D != Vector2.zero)
         {
+            
             Debug.Log("Pressing TouchPad" + Primary2D);
 
         }
+
+        
     }
 }
