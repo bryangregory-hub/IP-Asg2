1b530b6827ab9705ee53d1e469a9daede3a01a86
bryangregory-hub <73159622+bryangregory-hub@users.noreply.github.com>
2022-02-12 17:54:34 +0800

    adding button

--- IP asg 2/Assets/button/Scripts/HandButton.cs
+++ IP asg 2/Assets/button/Scripts/HandButton.cs
@@ -4,5 +4,51 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class HandButton : XRBaseInteractable
 {
+    private float yMin = 0.0f;
+    private float yMax = 0.0f;
+    private float previousHandHeight = 0.0f;
+    private XRBaseInteractor hoverInteractor = null;
+    protected override void Awake()
+    {
+        base.Awake();
+        onHoverEnter.AddListener(StartPress);
+        onHoverEnter.AddListener(EndPress);
+    }
+    private void OnDestroy()
+    {
+        onHoverEnter.RemoveListener(StartPress);
+        onHoverEnter.RemoveListener(EndPress);
+    }
+    private void StartPress(XRBaseInteractor interactor)
+    {
+        hoverInteractor = interactor;
+        previousHandHeight = interactor.transform.position.y;
+    }
+    private void EndPress(XRBaseInteractor interactor)
+    {
+        hoverInteractor = null;
+        previousHandHeight = 0.0f;
+    }
+    private void Start()
+    {
+        SetMinMax();
+    }
+    private void SetMinMax()
+    {
+        Collider collider = GetComponent<Collider>();
+        yMin = transform.position.y - (collider.bounds.size.y * 0.5f);
+        yMax = transform.position.y;
+    }
+    public override void ProcessInteractable(XRInteractionUpdateOrder.UpdatePhase updatePhase)
+    {
+        base.ProcessInteractable(updatePhase);
+    }
+    private float GetLocalYPosition(Vector3 position)
+    {
+        return 0.0f;
+    }
+    private void CheckPress()
+    {
 
+    }
 }
