4576f59e02907a42efb89a92c917ea1fde4580b7
bryangregory-hub <73159622+bryangregory-hub@users.noreply.github.com>
2022-02-15 06:31:55 +0800

    push piano

--- IP asg 2/Assets/Scripts/HandButton.cs
+++ IP asg 2/Assets/Scripts/HandButton.cs
@@ -4,5 +4,22 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class HandButton : XRBaseInteractable
 {
-   
+    public enum ButtonType
+    {
+        note,
+        piano
+    }
+    public ButtonType _buttonType;
+
+    public void Update()
+    {
+        switch(_buttonType)
+        {
+            case ButtonType.note:
+                break;
+            case ButtonType.piano:
+                break;
+        }
+    }
+
 }
