da2754a9890c36458bace4cb3d67596085ab22e6
Darrence622 <s10194154@connect.np.edu.sg>
2022-02-16 03:03:30 +0800

    QnA scripts

---
+++ IP asg 2/Assets/Quiz Script/QuizManager.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class QuizManager : MonoBehaviour
+{
+    // variable settings
+    public List<QuestionsAndAnswers> QnA;
+    public GameObject[] options;
+    public int currentQuestions;
+
+    public Text QuestionTxt;
+    public Text ScoreTxt;
+
+    int totalQuestion = 0;
+    public int score;
+
+    public GameObject quizPanel;
+    public GameObject GameOverPanel;
+
+    // counting question and generating steps
+    private void Start()
+    {
+        totalQuestion = QnA.Count;
+        GameOverPanel.SetActive(false);
+        generateQuestions();
+    }
+
+    // restart the game function
+    public void restart()
+    {
+        //SceneManager.LoadScene("Quiz");
+    }
+
+    // when game ends, lead players to game over scene
+    void GameOver()
+    {
+        quizPanel.SetActive(false);
+        GameOverPanel.SetActive(true);
+        ScoreTxt.text = score + "/" + totalQuestion;
+    }
+
+    // when players answer the question correctly
+    public void Correct()
+    {
+        score += 1; 
+        QnA.RemoveAt(currentQuestions);
+        generateQuestions();
+    }
+
+    // when players answers the question wrongly
+    public void wrong()
+    {
+        QnA.RemoveAt(currentQuestions);
+        generateQuestions();
+    }
+
+    // setting of answers for questions 
+    void SetAnswers()
+    {
+        for ( int i = 0; i < options.Length; i++)
+        {
+            options[i].GetComponent<AnswerScript>().isCorrect = false;
+            options[i].transform.GetChild(0).GetComponent<Text>().text = QnA[currentQuestions].Answers[i];
+
+            if (QnA[currentQuestions].CorrectAnswer == i + 1)
+            {
+                options[i].GetComponent<AnswerScript>().isCorrect = true;
+            }
+
+        }
+    }
+
+    // loading to mainmenu with the back to main menu button 
+    public void MainMenu()
+    {
+        //SceneManager.LoadScene("MainMenu");
+    }
+
+    // generating the questions 
+    void generateQuestions()
+    {
+        if (QnA.Count > 0)
+        {
+        currentQuestions = Random.Range(0, QnA.Count);
+
+        QuestionTxt.text = QnA[currentQuestions].Questions;
+
+        SetAnswers();
+
+        }
+        else
+        {
+            Debug.Log("Out of Questions");
+            GameOver();
+        }
+        
+    }
+}
